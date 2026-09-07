c0f04fa28355b2787d17422949bbe5a11a072e37
Michael Vanunu <veemikeyy@gmail.com>
2026-09-07 14:20:25 -0400

    Set up redis connection

--- app/core/config.py
+++ app/core/config.py
@@ -3,6 +3,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     database_url: str
+    redis_url: str = "redis://localhost:6379/0"
     secret_key: str
     algorithm: str =  "HS256"
     access_token_expire_minutes: int = 30
