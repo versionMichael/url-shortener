c4a58140ec5e7061b24015276590931c31452642
Michael Vanunu <veemikeyy@gmail.com>
2026-09-11 12:18:41 -0400

    Add basic Redis caching functions

--- app/services/cache.py
+++ app/services/cache.py
@@ -7,3 +7,21 @@ redis_client = redis.from_url(
     settings.redis_url,
     decode_responses=True
 )
+
+
+async def set_cached_url(
+    short_code: str,
+    original_url: str
+):
+    await redis_client.set(
+        f"url:{short_code}",
+        original_url
+    )
+
+
+async def get_cached_url(
+    short_code: str
+) -> str | None:
+    return await redis_client.get(
+        f"url:{short_code}"
+    )
