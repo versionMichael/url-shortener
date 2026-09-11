1a421d5a0ffd79473d827d59afe1a650eef08c7b
Michael Vanunu <veemikeyy@gmail.com>
2026-09-11 17:15:46 -0400

    make test db config optional

--- app/core/config.py
+++ app/core/config.py
@@ -3,7 +3,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     database_url: str
-    test_database_url: str
+    test_database_url: str | None = None
     redis_url: str = "redis://localhost:6379/0"
     secret_key: str
     algorithm: str =  "HS256"
